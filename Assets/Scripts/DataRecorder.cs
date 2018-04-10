b76f1fc8588513b0b07c4677d0f0a091cf4f9b07
U-DESKTOP-9DK85L9\Jackie <xieyubin@Umich.edu>
2018-04-09 23:23:03 -0400

    add car speed record script to record speed vs time

---
+++ Assets/Scripts/DataRecorder.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class DataRecorder : MonoBehaviour {
+
+    public string outputPath = "Assets/Data/speedData.txt";
+    public bool recording = true;
+
+    private Rigidbody carRB;
+    
+    //startTime : list of speed change at each delta time after start time
+    private Dictionary<float, List<float>> speedRecords;
+    private Dictionary<float, double> speedData = new Dictionary<float, double>();
+
+	// Use this for initialization
+	void Awake ()
+    {
+        carRB = GetComponentInParent<Rigidbody>();
+
+        if (carRB == null)
+            Debug.LogError("Error no rigbidbody attached to parent object");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown("q"))
+        {
+            storeData();
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if (recording && carRB)
+        {
+            float mph = carRB.velocity.magnitude * 2.237f;
+            
+            speedData.Add((float)Math.Round((double)Time.time, 2), (float)Math.Round((double)mph, 2) );
+        }
+    }
+
+
+    private void storeData()
+    {
+        recording = false;
+        //only perform at the end of the driving test
+        string path = outputPath;
+        StreamWriter writer = new StreamWriter(path, true);
+        foreach (KeyValuePair<float, double> speed in speedData)
+        {
+            writer.WriteLine(speed.Key.ToString("0.00") + "         "  + speed.Value.ToString("0.00"));
+        }
+
+        writer.Close();
+    }
+}
