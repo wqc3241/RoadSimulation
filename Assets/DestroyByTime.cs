f332a6869cf61d459d0a75517cf664bfc0091258
wqc3241 <qichaotomwang@gmail.com>
2018-04-13 00:37:03 -0400

    Random Car disappear after certain time

---
+++ Assets/DestroyByTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float delay = 15; //This implies a delay of 2 seconds.
+
+    void Start()
+    {
+        Destroy(gameObject,delay);
+    }
+}
