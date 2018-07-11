0e7355810bda70febeac64650c6a4546a82e3ec2
Yubing Xie <xieyubin@Umich.edu>
2018-07-11 15:15:35 +0800

    add click to start button

---
+++ Assets/StartScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour {
+    public string sceneName = "Scene_Yubing_stright";
+    public void StartSimulatorScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
